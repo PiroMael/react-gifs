930373b73e22962712119d8efdc2a05faa737346
Mael Piro <piromael46@gmail.com>
2022-01-18 15:23:52 +0100

    gif search work

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 import SearchBar from './components/searchbar';
 import Gif from './components/gif';
+import GifList from './components/giflist';
+import giphy from 'giphy-api';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = { 
+      gifs:[],
+      selectedGifId:"wK2MKn5jjcvUIe2ux1"
+     };
+     this.search("homer");
   }
 
+  search = (query) => {
+    giphy("zMvnOjXdAh3OY34PG7DJyPtQ89osASMx").search({
+      q:query,
+      rating:'g',
+      limit:10
+    }, (err,res) => {
+      this.setState({
+        gifs: res.data
+      });
+    });
+  }
   render() {
+    const gifs = [
+      { id: "wK2MKn5jjcvUIe2ux1" },
+      { id: "XX6nswKp9Olz1GIKrq" }
+    ];
     return (
       <div className="app">
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
           <div className="gif-list">
-            <Gif />
-            <Gif />
-            <Gif />
-            <Gif />
+            <GifList gifs={this.state.gifs} />
           </div>
         </div>
       </div>
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 
 class Gif extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  };
-  }
-
   render() {
+    const src =`https://media1.giphy.com/media/${this.props.id}/giphy.gif?cid=ecf05e475e18gjg5m0ijoa5bug3ei7210xhfe4kohlvqyvcv&rid=giphy.gif&ct=g`;
     return (
-      <img className="gif" src="https://media1.giphy.com/media/wK2MKn5jjcvUIe2ux1/giphy.gif?cid=ecf05e475e18gjg5m0ijoa5bug3ei7210xhfe4kohlvqyvcv&rid=giphy.gif&ct=g" alt="gif"/>
+      <img className="gif" src={src} alt="gif" />
     );
   }
 }
